167f690cbc4877a04d37a684526ab0593573d5ff
koppan21 <sn.solorzano.2020@alumnos.urjc.es>
2024-03-13 22:40:37 +0100

    Actualizacion servidor para distintos tipos MIME

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+
 const { error } = require('console');
 
 const PUERTO = 9090;
@@ -12,15 +13,39 @@ const server = http.createServer((req, res) => {
   const extension = path.extname(filePath);
   let contentType = 'text/html';
 
+  const extname = String(path.extname(filePath)).toLowerCase();
+
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+  };
+
   fs.readFile(filePath, (error, data) => {
     if (error) {
-      console.log("ERROR")
-      console.log(error.message);
-      // incluir angry server
-    } else {
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(data, 'utf8');
+      if (error.code === 'ENOENT') {
+        // Si el archivo no se encuentra, muestra un error 404
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('404 Not Found');
+        console.log("ERROR 404")
+        console.log(error.message);
+      } else {
+        // Si ocurre otro tipo de error, muestra un error 500
+        res.writeHead(500);
+        res.end(`Error: ${err.code}`);
+        console.log("ERROR 500")
+        console.log(error.message);
+      }
+      return;
     }
+    
+    const contentType = mimeTypes[extname] || 'application/octet-stream';
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data, 'utf-8');
   });
 });
 

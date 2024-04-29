f0d6abf37131400408fd2b16da228038e59d8715
koppan21 <sn.solorzano.2020@alumnos.urjc.es>
2024-04-29 23:39:11 +0200

    Configuración de la página de error 404

--- P1/tienda.js
+++ P1/tienda.js
@@ -2,35 +2,37 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const { error } = require('console');
-
 const PUERTO = 9090;
 
 //-- Servidor
 const server = http.createServer((req, res) => {
-  const url = req.url === '/' ? '/tienda.html' : req.url;
+  const url = req.url === '/' ? '/index.html' : req.url;
   const filePath = path.join(__dirname, url);
 
   const extname = String(path.extname(filePath)).toLowerCase();
 
   const mimeTypes = {
     '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.css': 'text/css',
+    '.js'  : 'text/javascript',
+    '.css' : 'text/css',
     '.json': 'application/json',
-    '.png': 'image/png',
-    '.jpg': 'image/jpg',
-    '.gif': 'image/gif',
+    '.png' : 'image/png',
+    '.jpg' : 'image/jpg',
+    'jpeg' : 'image/jpeg',
+    '.gif' : 'image/gif',
+    '.ico' : 'image/ico',
   };
 
   fs.readFile(filePath, (error, data) => {
     if (error) {
       if (error.code === 'ENOENT') {
         // Si el archivo no se encuentra, muestra un error 404
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('404 Not Found');
-        console.log("ERROR 404")
-        console.log(error.message);
+        fs.readFile('error404.html', (error, data) => {
+          const contentType = mimeTypes['.html'];
+          res.writeHead(404, { 'Content-Type': contentType });
+          res.end(data);
+          console.log("ERROR 404")
+        })
       } else {
         // Si ocurre otro tipo de error, muestra un error 500
         res.writeHead(500);
@@ -49,4 +51,4 @@ const server = http.createServer((req, res) => {
 
 //-- Activar el servidor
 server.listen(PUERTO);
-console.log("Tienda server activado!. Escuchando en puerto: " + PUERTO);
+console.log("Servidor de Tequila Temptations activado!. Escuchando en puerto: " + PUERTO);
